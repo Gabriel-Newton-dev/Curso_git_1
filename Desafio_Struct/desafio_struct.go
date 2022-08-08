38982e0e97a0ade26cccef906ffc6b369f05b6cb
Gabriel <gabriel@MacBook.local>
2022-08-07 22:50:08 -0700

    first commit

---
+++ Desafio_Struct/desafio_struct.go
@@ -0,0 +1,46 @@
+// faça duas Struct, uma usando "herança"
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type Carros struct {
+	Marca  string  `json:"Marca"`
+	Modelo string  `json:"Modelo"`
+	Valor  float64 `json:"Valor"`
+	Turbo  bool    `json:"Turbo"`
+}
+
+type Caminhonete struct {
+	Carros
+	Cabine    string `json:"Cabine"`
+	Importada bool   `json:"Importada"`
+}
+
+func main() {
+
+	Cruze := Carros{"Chevrolet", "Cruze Ltz", 98.060, true}
+
+	Ranger := Caminhonete{
+		Carros:    Carros{"Ford", "Ranger", 130.980, true},
+		Cabine:    "Dupla",
+		Importada: false,
+	}
+
+	cruzeJson, err := json.Marshal(Cruze)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(cruzeJson))
+
+	rangerJson, err := json.Marshal(Ranger)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(rangerJson))
+
+}
